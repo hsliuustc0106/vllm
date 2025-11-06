aedc13dd6be8d81d4e753eeedffc7c6c08c8bf72
CHEN <116010019@link.cuhk.edu.cn>
2025-11-06 18:54:19 +0800

    load mooncake config
    load mooncake config
    
    load mooncake config
    
    load mooncake config
    
    load mooncake config
    
    load mooncake config
    
    load mooncake store from config
    
    Signed-off-by: CHEN <116010019@link.cuhk.edu.cn>
    
    load mooncake store from config
    
    Signed-off-by: CHEN <116010019@link.cuhk.edu.cn>

--- vllm/distributed/ec_transfer/ec_lookup_buffer/mooncake_store.py
+++ vllm/distributed/ec_transfer/ec_lookup_buffer/mooncake_store.py
@@ -14,7 +14,7 @@ import os
 import threading
 from collections import deque
 from dataclasses import dataclass
-from typing import Optional, Any
+from typing import Any, Optional
 
 import numpy as np
 import regex as re
@@ -66,9 +66,9 @@ class MooncakeStoreConfig:
             local_hostname=config.get("local_hostname", "localhost"),
             metadata_server=config.get("metadata_server", ""),
             global_segment_size=config.get("global_segment_size",
-                                        DEFAULT_GLOBAL_SEGMENT_SIZE),
+                                           DEFAULT_GLOBAL_SEGMENT_SIZE),
             local_buffer_size=config.get("local_buffer_size",
-                                        DEFAULT_LOCAL_BUFFER_SIZE),
+                                         DEFAULT_LOCAL_BUFFER_SIZE),
             protocol=config.get("protocol", "tcp"),
             device_name=config.get("device_name", ""),
             master_server_address=config.get("master_server_address", ""),
